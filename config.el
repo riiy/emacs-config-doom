4e87ee0f129b68e019aabd70daaf5d24f018fca1
riiy <riiyzhou@gmail.com>
2023-05-18 06:41:44 +0000

    delete cc && org

--- config.el
+++ config.el
@@ -40,68 +40,6 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-(use-package org-roam
-  :ensure t
-  :custom
-  (org-roam-directory (file-truename "~/org-roam/"))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
-  :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
-  (require 'org-roam-protocol))
-
-(setq find-file-visit-truename t)
-;; for org-roam-buffer-toggle
-;; Use side-window like V1
-;; This can take advantage of slots available with it
-(add-to-list 'display-buffer-alist
-    '("\\*org-roam\\*"
-        (display-buffer-in-side-window)
-        (side . right)
-        (slot . 0)
-        (window-width . 0.25)
-        (preserve-size . (t . nil))
-        (window-parameters . ((no-other-window . t)
-                              (no-delete-other-windows . t)))))
-(use-package! org-roam-protocol
-  :after org-protocol)
-
-(setq org-roam-capture-templates
-      '(
-        ("d" "default" plain "%?"
-         :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                            "#+title: ${title}\n#+roam_alias:\n#+roam_key:\n#+roam_tags:\n\n")
-         :unnarrowed t)
-        )
-      )
-
-(setq org-roam-capture-ref-templates
-      '(
-        ("a" "Annotation" plain
-         "%U ${body}\n"
-         :target (file+head "${slug}.org"
-                            "#+title: ${title}\n#+roam_key: ${ref}\n#+roam_alias:\n#+roam_tags:\n\n")
-         :immediate-finish t
-         :unnarrowed t
-         )
-        ("r" "ref" plain ""
-         :target (file+head "${slug}.org"
-                            "#+title: ${title}\n#+roam_key: ${ref}\n#+roam_alias:\n#+roam_tags:\n\n")
-         :immediate-finish t
-         :unnarrowed t)
-        )
-      )
-
-(set-eglot-client! 'cc-mode '("clangd" "-j=3" "--clang-tidy"))
 ;; input
 (setq default-input-method "pyim")
 (setq pyim-default-scheme 'wubi)
