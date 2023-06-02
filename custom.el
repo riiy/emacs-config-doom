74673ebdee1320fd4b822fe1d6d995556eebbc9c
riiy zhou <riiyzhou@gmail.com>
2023-06-02 23:36:59 +0800

    add org-reset-subtask

---
+++ custom.el
@@ -0,0 +1,46 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yasnippet-classic-snippets yasnippet mu4easy smtpmail-multi mu4e-maildirs-extension magit-section anaconda-mode))
+ '(send-mail-function 'smtpmail-send-it))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq org-default-properties (cons "RESET_SUBTASKS" org-default-properties))
+
+(defun org-reset-subtask-state-subtree ()
+  "Reset all subtasks in an entry subtree."
+  (interactive "*")
+  (if (org-before-first-heading-p)
+      (error "Not inside a tree")
+    (save-excursion
+      (save-restriction (org-narrow-to-subtree)
+        (org-fold-show-subtree) (goto-char (point-min))
+        (beginning-of-line 2)
+        (narrow-to-region (point) (point-max))
+        (org-map-entries
+         '(when (member (org-get-todo-state) org-done-keywords)
+            (org-todo (car org-todo-keywords))))
+        ))))
+
+(defun org-reset-subtask-state-maybe ()
+  "Reset all subtasks in an entry if the `RESET_SUBTASKS' property is set"
+  (interactive "*")
+  (if (org-entry-get (point) "RESET_SUBTASKS")
+      (org-reset-subtask-state-subtree)))
+
+(defun org-subtask-reset ()
+  (when (member org-state org-done-keywords) ;; org-state dynamically bound in org.el/org-todo
+    (org-reset-subtask-state-maybe)
+    (org-update-statistics-cookies t)))
+
+(add-hook 'org-after-todo-state-change-hook 'org-subtask-reset)
+
+(provide 'org-subtask-reset)
