58036691e67d9dafc611d5b070993bdcc30cf867
riiy zhou <riiyzhou@gmail.com>
2023-06-09 14:07:45 +0800

    update default target language code for zh zh-CN

--- config.el
+++ config.el
@@ -184,4 +184,4 @@
 (global-set-key "\C-ct" 'google-translate-at-point)
 (global-set-key "\C-cT" 'google-translate-query-translate)
 (setq google-translate-default-source-language "en")
-(setq google-translate-default-target-language "zh")
+(setq google-translate-default-target-language "zh-CN")
