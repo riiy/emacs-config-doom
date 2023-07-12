75c7f723f053f7c221df23edb8b942b3efc6d884
riiy zhou <riiyzhou@gmail.com>
2023-07-12 18:18:57 +0800

    update settings of w3m

--- config.el
+++ config.el
@@ -195,7 +195,6 @@
       "https://translate.google.com/")
 
 ;; w3m
- (setq browse-url-browser-function 'w3m-browse-url)
- (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
- ;; optional keyboard short-cut
- (global-set-key "\C-xm" 'browse-url-at-point)
+(setq browse-url-browser-function 'w3m-browse-url)
+(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+(global-set-key "\C-xm" 'browse-url-at-point) ;; optional keyboard short-cut
